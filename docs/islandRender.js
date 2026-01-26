19a1281fa5fc91d35211fe46af63a2c5af4b5502
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2026-01-26 19:17:20 -0300

    remove hover bg

--- docs/islandRender.js
+++ docs/islandRender.js
@@ -25887,9 +25887,20 @@ function MenuDrawer() {
               href: "https://www.linkedin.com/in/palmeida-dev/",
               target: "_blank",
               rel: "noopener noreferrer",
-              className: "p-3 text-white/70 hover:text-white hover:bg-white/10 rounded-lg transition-all",
+              className: "p-3 text-white/70 hover:text-white rounded-lg transition-all hover:scale-110 hover:rotate-12",
               "aria-label": "LinkedIn",
-              children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("svg", { role: "img", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", fill: "currentColor", children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("path", { d: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" }) })
+              children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)(
+                "svg",
+                {
+                  role: "img",
+                  viewBox: "0 0 24 24",
+                  xmlns: "http://www.w3.org/2000/svg",
+                  width: "24",
+                  height: "24",
+                  fill: "currentColor",
+                  children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("path", { d: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" })
+                }
+              )
             }
           ),
           /* @__PURE__ */ (0, import_jsx_runtime10.jsx)(
@@ -25898,9 +25909,20 @@ function MenuDrawer() {
               href: "https://github.com/PedroMarianoAlmeida",
               target: "_blank",
               rel: "noopener noreferrer",
-              className: "p-3 text-white/70 hover:text-white hover:bg-white/10 rounded-lg transition-all",
+              className: "p-3 text-white/70 hover:text-white rounded-lg transition-all hover:scale-110 hover:rotate-12",
               "aria-label": "GitHub",
-              children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("svg", { role: "img", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", fill: "currentColor", children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("path", { d: siGithub.path }) })
+              children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)(
+                "svg",
+                {
+                  role: "img",
+                  viewBox: "0 0 24 24",
+                  xmlns: "http://www.w3.org/2000/svg",
+                  width: "24",
+                  height: "24",
+                  fill: "currentColor",
+                  children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("path", { d: siGithub.path })
+                }
+              )
             }
           ),
           /* @__PURE__ */ (0, import_jsx_runtime10.jsx)(
@@ -25908,9 +25930,20 @@ function MenuDrawer() {
             {
               href: "./assets/pedro-almeida-cv.docx",
               download: true,
-              className: "p-3 text-white/70 hover:text-white hover:bg-white/10 rounded-lg transition-all",
+              className: "p-3 text-white/70 hover:text-white rounded-lg transition-all hover:scale-110 hover:rotate-12",
               "aria-label": "Download CV",
-              children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("svg", { role: "img", viewBox: "0 0 24 24", xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", fill: "currentColor", children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("path", { d: siReaddotcv.path }) })
+              children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)(
+                "svg",
+                {
+                  role: "img",
+                  viewBox: "0 0 24 24",
+                  xmlns: "http://www.w3.org/2000/svg",
+                  width: "24",
+                  height: "24",
+                  fill: "currentColor",
+                  children: /* @__PURE__ */ (0, import_jsx_runtime10.jsx)("path", { d: siReaddotcv.path })
+                }
+              )
             }
           )
         ] }) })
